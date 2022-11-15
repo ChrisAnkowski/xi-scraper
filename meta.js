5a6d634a048f3c3baa363e7f107dc2901e7689e3
Christian Ankowski <christian.ankowski@flaschenpost.de>
2022-11-15 21:21:31 +0100

    added user and meta script

---
+++ meta.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @name        XI Scraper
+// @version     1.0.0
+// @author      Christian Ankowski
+// @downloadURL https://github.com/ChrisAnkowski/xi-scraper/raw/main/user.js
+// @updateURL   https://github.com/ChrisAnkowski/xi-scraper/raw/main/meta.js
+// ==/UserScript==
---
+++ user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name        XI Scraper
+// @version     1.0.0
+// @author      Christian Ankowski
+// @match       *://*.xing.com/profile/*
+// @run-at      document-start
+// @downloadURL https://github.com/ChrisAnkowski/xi-scraper/raw/main/user.js
+// @updateURL   https://github.com/ChrisAnkowski/xi-scraper/raw/main/meta.js
+// ==/UserScript==
