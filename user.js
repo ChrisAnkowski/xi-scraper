188044d3fbe46aa14071776134194c344d3b4195
Christian Ankowski <christian.ankowski@flaschenpost.de>
2022-11-15 23:04:37 +0100

    wip

--- user.js
+++ user.js
@@ -23,18 +23,18 @@ async function loadBundle() {
 
     const commit = (await (await fetch(commitURL, { cache: 'no-store' })).json())[0].commit;
     const commitDate = new Date(commit.committer.date);
-
+    console.log(commitDate > lastModified);
     if (commitDate > lastModified) return updateBundle(commitDate);
 
     return GM_getValue('bundle');
 }
 
 async function updateBundle(date) {
-    const bundleURL = 'https://raw.githubusercontent.com/ChrisAnkowski/xi-scraper/main/bundle.js';
+    const bundleURL = 'https://raw.githubusercontent.com/ChrisAnkowski/xi-scraper/main/bundle.js?=cache=' + new Date().getTime() + Math.random();
     const bundle = await (await fetch(bundleURL, { cache: 'no-store' })).text();
 
     GM_setValue('bundle', bundle);
-    GM_setValue('lastModified', date);
+    GM_setValue('lastModified', date.toString());
 
     return bundle;
 }
