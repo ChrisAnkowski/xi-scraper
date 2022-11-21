f998980277fb200175fc06cf391cf80c2b7db097
Christian Ankowski <christian.ankowski@flaschenpost.de>
2022-11-21 09:56:20 +0100

    wip

--- user.js
+++ user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
-// @name        XI Scraper
-// @version     1.0.2
-// @description XI Scraper
-// @author      Christian Ankowski
-// @match       https://xing.com/profile/*
-// @run-at      document-end
-// @grant       GM_setValue
-// @grant       GM_getValue
-// @downloadURL https://github.com/ChrisAnkowski/xi-scraper/raw/main/user.js
-// @updateURL   https://github.com/ChrisAnkowski/xi-scraper/raw/main/meta.js
+// @name         XI Scraper
+// @namespace    http://tampermonkey.net/
+// @version      1.0.3
+// @description  XI Scraper
+// @author       Christian Ankowski
+// @match        https://www.xing.com/profile/*
+// @downloadURL  https://github.com/ChrisAnkowski/xi-scraper/raw/main/user.js
+// @updateURL    https://github.com/ChrisAnkowski/xi-scraper/raw/main/meta.js
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=xing.com
+// @grant        none
 // ==/UserScript==
 
 const bundle = loadBundle();
