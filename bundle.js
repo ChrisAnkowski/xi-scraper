785fa2e231b0d9369bbd29d85959057fbedbaec9
Christian Ankowski <christian.ankowski@flaschenpost.de>
2022-11-22 11:46:59 +0100

    wip

--- bundle.js
+++ bundle.js
@@ -1,153 +1,207 @@
-// Vorname ✅
-// Nachname ✅
-// Aktuelle Position ✅
-// Gehaltswunsch ✅
-// Berufserfahrung
-// Ausbildung / Qualifikation
-// Ort
-
-
-// const string = url;
-// const regexp = /(\d{0,2}) Jahr/g;
-// const matches = string.matchAll(regexp);
-// let test = [];
-// for (const match of matches) {
-// 	test.push(match[1]);
-// }
-
 let userData = {};
 ready(() => {
-    const button = document.createElement('button');
-    button.innerText = 'Profilinformationen kopieren';
-    button.onclick = scrapeInformation;
-    const moreButton = document.querySelector('*[data-qa="more-button"]');
-    insertAfter(button, moreButton);
+  const button = document.createElement("button");
+  button.innerText = "Profilinformationen kopieren";
+  button.onclick = scrapeInformation;
+  const moreButton = document.querySelector('*[data-qa="more-button"]');
+  insertAfter(button, moreButton);
 });
 
+function scrapeInformation() {
+  searchForName();
+  searchForSalaryWish();
+  searchForJobTitle();
+  getJobExpirience();
+  getLocation();
+  getJobExpirience();
+
+  copyTextToClipboard(JSON.stringify(userData));
+}
+
 function getLocation() {
-    const locationPin = document.querySelector('*[data-xds="IconLocationPin"]');
-    const locationString = locationPin.parentNode.lastElementChild.textContent;
-    addInformationToResult('city', locationString);
+  const locationPin = document.querySelector('*[data-xds="IconLocationPin"]');
+  const locationString = locationPin.parentNode.lastElementChild.textContent;
+  addInformationToResult("city", locationString);
 }
 
 function extractYearsAsMonths(string) {
-    const regexp = /(\d{0,2}) Jahr/g;
-    const matches = string.matchAll(regexp);
-    let months = [];
-    for (const match of matches) {
-    	months.push(Number(match[1]) * 12);
-    }
-    return months
+  const regexp = /(\d{0,2}) Jahr/g;
+  const matches = string.matchAll(regexp);
+  let months = [];
+  for (const match of matches) {
+    months.push(Number(match[1]) * 12);
+  }
+  return months;
 }
 
 function extractMonths(string) {
-    const regexp = /(\d{0,2}) Monat/g;
-    const matches = string.matchAll(regexp);
-    let months = [];
-    for (const match of matches) {
-    	months.push(Number(match[1]));
-    }
-    return months
+  const regexp = /(\d{0,2}) Monat/g;
+  const matches = string.matchAll(regexp);
+  let months = [];
+  for (const match of matches) {
+    months.push(Number(match[1]));
+  }
+  return months;
+}
+
+function getEducation() {
+  const section = document.getElementById("ProfileTimelineModule");
+  const headline = [...section.querySelectorAll('*[data-xds="Headline"]')]
+    .filter((element) => element.innerText.includes("Studium"))
+    .at(0);
+  const education = headline.nextElementSibling.querySelector("h2").innerText;
+  addInformationToResult("education", education);
 }
 
 function getJobExpirience() {
-    const section = document.getElementById('ProfileTimelineModule');
-    const headline = [...section.querySelectorAll('*[data-xds="Headline"]')].filter(element => element.innerText.includes("Berufliche Stationen")).at(0);
-    const ersteStationen = [...headline.parentNode.querySelectorAll(':scope > div')];
-    const mehrAnzeigenStationen = [...headline.parentNode.nextSibling.querySelectorAll('*[data-qa="bucket"]:first-child > div')];
-    let jobDuration = [];
-    ersteStationen.forEach((element) => {
-        jobDuration.push(element.querySelector('p[data-xds="BodyCopy"]').nextElementSibling.innerText);
-    })
-    mehrAnzeigenStationen.forEach((element) => {
-        jobDuration.push(element.querySelector('p[data-xds="BodyCopy"]').nextElementSibling.innerText);
-    })
-    const extractedYearsAsMonths = extractYearsAsMonths(jobDuration.join(' '));
-    const extractedMonths = extractMonths(jobDuration.join(' '));
-    const expirience = extractedYearsAsMonths.concat(extractedMonths).reduce((partialSum, a) => partialSum + a, 0);
-    addInformationToResult('expirience', getYearsString(expirience));
+  const section = document.getElementById("ProfileTimelineModule");
+  const headline = [...section.querySelectorAll('*[data-xds="Headline"]')]
+    .filter((element) => element.innerText.includes("Berufliche Stationen"))
+    .at(0);
+  const ersteStationen = [
+    ...headline.parentNode.querySelectorAll(":scope > div"),
+  ];
+  const mehrAnzeigenStationen = [
+    ...headline.parentNode.nextSibling.querySelectorAll(
+      '*[data-qa="bucket"]:first-child > div'
+    ),
+  ];
+  let jobDuration = [];
+  ersteStationen.forEach((element) => {
+    jobDuration.push(
+      element.querySelector('p[data-xds="BodyCopy"]').nextElementSibling
+        .innerText
+    );
+  });
+  mehrAnzeigenStationen.forEach((element) => {
+    jobDuration.push(
+      element.querySelector('p[data-xds="BodyCopy"]').nextElementSibling
+        .innerText
+    );
+  });
+  const extractedYearsAsMonths = extractYearsAsMonths(jobDuration.join(" "));
+  const extractedMonths = extractMonths(jobDuration.join(" "));
+  const expirience = extractedYearsAsMonths
+    .concat(extractedMonths)
+    .reduce((partialSum, a) => partialSum + a, 0);
+  addInformationToResult("expirience", getYearsString(expirience));
 }
 
 function getYearsString(monthCount) {
-    function getPlural(number, word) {
-        return number === 1 && word.one || word.other;
-    }
+  function getPlural(number, word) {
+    return (number === 1 && word.one) || word.other;
+  }
 
-    var months = { one: 'Monat', other: 'Monate' },
-        years = { one: 'Jahr', other: 'Jahre' },
-        m = monthCount % 12,
-        y = Math.floor(monthCount / 12),
-        result = [];
+  var months = { one: "Monat", other: "Monate" },
+    years = { one: "Jahr", other: "Jahre" },
+    m = monthCount % 12,
+    y = Math.floor(monthCount / 12),
+    result = [];
 
-    y && result.push(y + ' ' + getPlural(y, years));
-    m && result.push(m + ' ' + getPlural(m, months));
-    return result.join(' und ');
-}
-
-function scrapeInformation() {
-    searchForName();
-    searchForSalaryWish();
-    searchForJobTitle();
-    getJobExpirience();
-    getLocation();
-    getJobExpirience();
-    console.log(userData);
+  y && result.push(y + " " + getPlural(y, years));
+  m && result.push(m + " " + getPlural(m, months));
+  return result.join(" und ");
 }
 
 function searchForSalaryWish() {
-    const allH2Array = [...document.querySelectorAll("h2")];
-    const salaryHeadline = allH2Array.filter((element) => element.textContent.includes("Gehaltsvorstellung"));
-    if (salaryHeadline.length > 0) {
-        const salaryHeadlineSibling = salaryHeadline[0].nextElementSibling;
-        const salaryWish = salaryHeadlineSibling.textContent;
-        addInformationToResult('salary', salaryWish);
-    } else {
-        addInformationToResult('salary', 'Nicht angegeben');
-    }
+  const allH2Array = [...document.querySelectorAll("h2")];
+  const salaryHeadline = allH2Array.filter((element) =>
+    element.textContent.includes("Gehaltsvorstellung")
+  );
+  if (salaryHeadline.length > 0) {
+    const salaryHeadlineSibling = salaryHeadline[0].nextElementSibling;
+    const salaryWish = salaryHeadlineSibling.textContent;
+    addInformationToResult("salary", salaryWish);
+  } else {
+    addInformationToResult("salary", "Nicht angegeben");
+  }
 }
 
 function searchForName() {
-    const name = document.querySelector('#XingIdModule *[data-xds="Hero"]').innerText.replace(/<(.|\n)*?>/g, '').split('\n')[0].trim();
-    const nameSplit = name.split(' ');
-    let nachname = nameSplit.pop();
-    let vorname = typeof nameSplit === 'string' ? nameSplit : nameSplit.join(' ');
-    addInformationToResult('firstname', vorname);
-    addInformationToResult('lastname', nachname);
+  const name = document
+    .querySelector('#XingIdModule *[data-xds="Hero"]')
+    .innerText.replace(/<(.|\n)*?>/g, "")
+    .split("\n")[0]
+    .trim();
+  const nameSplit = name.split(" ");
+  let nachname = nameSplit.pop();
+  let vorname = typeof nameSplit === "string" ? nameSplit : nameSplit.join(" ");
+  addInformationToResult("firstname", vorname);
+  addInformationToResult("lastname", nachname);
 }
 
 function searchForJobTitle() {
-    const jobTitleString = document.querySelector('#XingIdModule *[data-xds="Hero"]').parentElement.nextElementSibling.querySelector('section p').textContent.split(',');
-    let jobTitle = jobTitleString[1];
-    if (jobTitleString.includes('Student')) {
-        jobTitle = jobTitleString[0] + ' ' + jobTitleString[1];
-    }
-    addInformationToResult('currentJob', jobTitle.trim());
+  const jobTitleString = document
+    .querySelector('#XingIdModule *[data-xds="Hero"]')
+    .parentElement.nextElementSibling.querySelector("section p")
+    .textContent.split(",");
+  let jobTitle = jobTitleString[1];
+  if (jobTitleString.includes("Student")) {
+    jobTitle = jobTitleString[0] + " " + jobTitleString[1];
+  }
+  addInformationToResult("currentJob", jobTitle.trim());
 }
 
 function addInformationToResult(key, info) {
-    userData[key] = info;
+  userData[key] = info;
 }
 
 function insertAfter(newNode, referenceNode) {
-    if (referenceNode && referenceNode.parentNode) {
-        referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
-    }
+  if (referenceNode && referenceNode.parentNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+  }
 }
 
 function ready(fn) {
-    if (document.readyState === "complete" || document.readyState === "interactive") {
-        setTimeout(fn, 1500);
-    } else {
-        document.addEventListener("DOMContentLoaded", fn);
-    }
+  if (
+    document.readyState === "complete" ||
+    document.readyState === "interactive"
+  ) {
+    setTimeout(fn, 1500);
+  } else {
+    document.addEventListener("DOMContentLoaded", fn);
+  }
 }
 
-if( typeof Element.prototype.clearChildren === 'undefined' ) {
-    Object.defineProperty(Element.prototype, 'clearChildren', {
-        configurable: true,
-        enumerable: false,
-        value: function() {
-            while(this.firstChild) this.removeChild(this.lastChild);
-        }
-    });
+if (typeof Element.prototype.clearChildren === "undefined") {
+  Object.defineProperty(Element.prototype, "clearChildren", {
+    configurable: true,
+    enumerable: false,
+    value: function () {
+      while (this.firstChild) this.removeChild(this.lastChild);
+    },
+  });
+}
+
+function fallbackCopyTextToClipboard(text) {
+  var textArea = document.createElement("textarea");
+  textArea.value = text;
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
+
+  try {
+    var successful = document.execCommand("copy");
+    var msg = successful ? "successful" : "unsuccessful";
+    console.log("Fallback: Copying command was " + msg);
+  } catch (err) {
+    console.error("Fallback: Oops, unable to copy", err);
+  }
+
+  document.body.removeChild(textArea);
+}
+
+function copyTextToClipboard(text) {
+  if (!navigator.clipboard) {
+    fallbackCopyTextToClipboard(text);
+    return;
+  }
+  navigator.clipboard.writeText(text).then(
+    function () {
+      console.log("Async: Copying to clipboard was successful!");
+    },
+    function (err) {
+      console.error("Async: Could not copy text: ", err);
+    }
+  );
 }
---
+++ crm.js
@@ -0,0 +1,109 @@
+let data = {};
+ready(() => {
+  const wrapper = document.createElement("div");
+  const input = document.createElement("input");
+  const label = document.createElement("label");
+  const inputWrapper = document.createElement("div");
+  input.id = "information_json";
+  input.oninput = inputChange;
+  input.style.width = '100%';
+  label.htmlFor = "information_json";
+  label.innerText = "Profilinformationen JSON";
+  wrapper.appendChild(label);
+  inputWrapper.appendChild(input);
+  wrapper.appendChild(inputWrapper);
+  insertAfter(wrapper, document.querySelector(".card-content h2"));
+});
+
+let fieldMap = {
+  'Vorname': 'firstname',
+  'Nachname': 'lastname',
+  'Gehaltswunsch': 'salary',
+  'Aktuelle Position': 'currentJob',
+  'Berufserfahrung': 'expirience',
+  'Ort': 'city'
+};
+
+function objectEquals(x, y) {
+  'use strict';
+
+  if (x === null || x === undefined || y === null || y === undefined) { return x === y; }
+  // after this just checking type of one would be enough
+  if (x.constructor !== y.constructor) { return false; }
+  // if they are functions, they should exactly refer to same one (because of closures)
+  if (x instanceof Function) { return x === y; }
+  // if they are regexps, they should exactly refer to same one (it is hard to better equality check on current ES)
+  if (x instanceof RegExp) { return x === y; }
+  if (x === y || x.valueOf() === y.valueOf()) { return true; }
+  if (Array.isArray(x) && x.length !== y.length) { return false; }
+
+  // if they are dates, they must had equal valueOf
+  if (x instanceof Date) { return false; }
+
+  // if they are strictly equal, they both need to be object at least
+  if (!(x instanceof Object)) { return false; }
+  if (!(y instanceof Object)) { return false; }
+
+  // recursive object equality check
+  var p = Object.keys(x);
+  return Object.keys(y).every(function (i) { return p.indexOf(i) !== -1; }) &&
+      p.every(function (i) { return objectEquals(x[i], y[i]); });
+}
+
+function inputChange(event) {
+  const parsedValue = JSON.parse(event.target.value);
+  if (!objectEquals(parsedValue, data)) {
+    data = parsedValue;
+  }
+  fillInForm();
+}
+
+function findAncestor (el, sel) {
+  while ((el = el.parentElement) && !((el.matches || el.matchesSelector).call(el,sel)));
+  return el;
+}
+
+function fillInForm() {
+  const allInputs = [...document.querySelectorAll('#card-Allgemeines section input')];
+  allInputs.forEach((input) => {
+    const labelName = findAncestor(input, '.field').innerText;
+    const labelKey = fieldMap[labelName];
+    const dataValue = data[labelKey];
+    if (!dataValue) {
+      return
+    }
+    input.value = dataValue;
+  });
+}
+
+async function getClipboardContent() {
+  try {
+    const permission = await navigator.permissions.query({
+      name: "clipboard-read",
+    });
+    if (permission.state === "denied") {
+      throw new Error("Not allowed to read clipboard.");
+    }
+    const clipboardContents = await navigator.clipboard.read();
+    console.log(clipboardContents);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+function ready(fn) {
+  if (
+    document.readyState === "complete" ||
+    document.readyState === "interactive"
+  ) {
+    setTimeout(fn, 1500);
+  } else {
+    document.addEventListener("DOMContentLoaded", fn);
+  }
+}
+
+function insertAfter(newNode, referenceNode) {
+  if (referenceNode && referenceNode.parentNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+  }
+}
