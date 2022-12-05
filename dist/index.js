b7177e121e0801a1316f27e02535c59ad68ed988
Christian Ankowski <christian.ankowski@flaschenpost.de>
2022-12-05 14:58:34 +0100

    wip

--- dist/index.js
+++ dist/index.js
@@ -383,13 +383,52 @@ class Controller {
     return window.location.href;
   }
 }
-ready(() => {
+(function(funcName, baseObj) {
+  funcName = funcName || "docReady";
+  baseObj = baseObj || window;
+  let readyList = [];
+  let readyFired = false;
+  let readyEventHandlersInstalled = false;
+  function ready() {
+    if (!readyFired) {
+      readyFired = true;
+      for (let i = 0; i < readyList.length; i++) {
+        readyList[i].fn.call(window, readyList[i].ctx);
+      }
+      readyList = [];
+    }
+  }
+  function readyStateChange() {
+    if (document.readyState === "complete") {
+      ready();
+    }
+  }
+  baseObj[funcName] = function(callback, context) {
+    if (typeof callback !== "function") {
+      throw new TypeError("callback for docReady(fn) must be a function");
+    }
+    if (readyFired) {
+      setTimeout(function() {
+        callback(context);
+      }, 1);
+      return;
+    } else {
+      readyList.push({ fn: callback, ctx: context });
+    }
+    if (document.readyState === "complete" || !document.attachEvent && document.readyState === "interactive") {
+      setTimeout(ready, 1);
+    } else if (!readyEventHandlersInstalled) {
+      if (document.addEventListener) {
+        document.addEventListener("DOMContentLoaded", ready, false);
+        window.addEventListener("load", ready, false);
+      } else {
+        document.attachEvent("onreadystatechange", readyStateChange);
+        window.attachEvent("onload", ready);
+      }
+      readyEventHandlersInstalled = true;
+    }
+  };
+})("docReady", window);
+docReady(function() {
   new Controller();
 });
-function ready(eventListener) {
-  if (document.readyState === "complete" || document.readyState === "interactive") {
-    setTimeout(eventListener, 1500);
-  } else {
-    document.addEventListener("DOMContentLoaded", eventListener);
-  }
-}
