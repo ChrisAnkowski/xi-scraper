9ec75d012da630538510d0c2a7ec5b28ad3d5e8d
Christian Ankowski <christian.ankowski@flaschenpost.de>
2022-12-05 10:34:49 +0100

    wip

--- index.ts
+++ index.ts
@@ -1,3 +1,15 @@
+// ==UserScript==
+// @name         XI Scraper
+// @namespace    http://tampermonkey.net/
+// @version      1.0.3
+// @description  XI Scraper
+// @author       Christian Ankowski
+// @match        https://www.xing.com/profile/*
+// @match        https://www.linkedin.com/in/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=xing.com
+// @grant        none
+// ==/UserScript==
+
 // @ts-nocheck
 import Controller from './Classes/Controller';
 
